11c588d0821fd79d8f51a70999d37542ef4e614d
dhkima <dhkima@higenmotor.com>
2021-12-30 11:01:54 +0900

    Simplified Execute

--- CRD3.bat
+++ CRD3.bat
@@ -2,11 +2,12 @@ REM Anaconda3 Environment
 REM set root=%USERPROFILE%\anaconda3
 set root=%ProgramData%\anaconda3
 call %root%\Scripts\activate.bat %root%
-call conda env list
+REM call conda env list
 call conda activate base
 
 REM Start App
 call cd /d D:\git\CRD3\
 call python CRD3.py
 
-pause
+REM pause
+exit
